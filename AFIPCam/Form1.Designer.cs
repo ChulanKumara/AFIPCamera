6d4dab18648866cf6afbcd2d066a3cccadccecbb
chulan.kumara <chulan.kumara@DESKTOP-4VO3P04>
2017-09-18 16:03:58 +0530

    Add project files.

---
+++ AFIPCam/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AFIPCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 461);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(717, 393);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 1;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(717, 450);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(75, 23);
+            this.button_Stop.TabIndex = 2;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 498);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Stop;
+    }
+}
+
---
+++ AFIPCam/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+
+namespace AFIPCam
+{
+    public partial class Form1 : Form
+    {
+        private MJPEGStream stream;
+        private Byte [] buffer;
+        private int total = 0;
+        private int read;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Link to IP Cam feed
+            var sourceURL = "http://10.128.1.31/Streaming/Channels/1/picture";
+            //var sourceURL = "http://88.53.197.250/axis-cgi/mjpg/video.cgi?resolution=320×240";           
+
+            stream = new MJPEGStream(sourceURL);
+            stream.Login = "admin";
+            stream.Password = "admin1234";
+            stream.NewFrame += stream_NewFrame;
+
+            buffer = new byte[100000];
+
+
+            // Create HTTP request
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(sourceURL);
+
+            // Provide credentials for connection
+            req.Credentials = new NetworkCredential("admin", "admin1234");
+
+            // Get response
+            WebResponse resp = req.GetResponse();
+
+            // Get response stream
+            Stream streamer = resp.GetResponseStream();
+
+            // Read data from stream
+            while ((read = streamer.Read(buffer, total, 1000)) != 0)
+            {
+                total = total + read;
+            }
+        }
+
+        void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            //pictureBox1.Image = bmp;
+
+
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                stream.Start();
+
+                // Set image to picturebox
+                Bitmap bmp = (Bitmap)Bitmap.FromStream(
+                    new MemoryStream(buffer, 0, total));
+
+                pictureBox1.Image = bmp;
+                //pictureBox1.Invalidate();
+                //pictureBox1.Refresh();
+                //pictureBox1.Update();
+
+                MessageBox.Show("Streaming Started!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+                throw;
+            }          
+            pictureBox1.Refresh();
+            pictureBox1.Invalidate();
+            Thread.Sleep(1000);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                
+                if (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // user clicked yes
+                    stream.Stop();
+                    this.Dispose();
+                }
+                else
+                {
+                    // user clicked no
+                    stream.Start();
+                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+                throw;
+            }
+
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stream.Stop();
+                MessageBox.Show("Streaming Stopped!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+                throw;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stream.Start();
+        }
+    }
+}
